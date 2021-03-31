22a9ffb3cfc4202c3252ee695a64df5871ee36ea
Kawhi <zcb119@foxmail.com>
2021-03-31 09:43:02 +0800

    --story=add detail tableview

--- Demo-FoodPin/YQRestaurant/Favorites/ViewController/YQRestautrantDetailViewController.swift
+++ Demo-FoodPin/YQRestaurant/Favorites/ViewController/YQRestautrantDetailViewController.swift
@@ -12,17 +12,68 @@ private struct Constants {
 
 import UIKit
 
-class YQRestautrantDetailViewController: UIViewController {
+class YQRestautrantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
-    var restaurant: RestaurantMO!
-
-    var detailTabelView : UITableView!
-    var headerView : UIImageView!
-    var headerMaskView : UIView!
-    var nameLabel : UILabel!
-    var typeLabel : UILabel!
-    var headerImageView : UIImageView!
-    var ratingImageView : UIImageView!
+    public var restaurant: RestaurantMO!
+    
+    private lazy var detailTableView: UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.delegate = self
+        table.separatorStyle = .none
+        table.register(YQRestaurantDetailIconTextCell.self, forCellReuseIdentifier: String(describing: YQRestaurantDetailIconTextCell.self))
+        table.register(YQReataurantDetailMapCell.self, forCellReuseIdentifier: String(describing: YQReataurantDetailMapCell.self))
+        table.register(YQReataurantDetailTextCell.self, forCellReuseIdentifier: String(describing: YQReataurantDetailTextCell.self))
+        table.contentInsetAdjustmentBehavior = .never
+        return table
+    }()
+    
+    private lazy var headerView: UIImageView = {
+        let header = UIImageView()
+        header.contentMode = .scaleAspectFill
+        header.layer.masksToBounds = true
+    }()
+    
+    private lazy var headerMaskView: UIView = {
+        let mask = UIView()
+        mask.backgroundColor = .black
+        mask.alpha = 0.2
+        return mask
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 35)
+        label.text = restaurant.name
+        label.textColor = .white
+        label.sizeToFit()
+        return label
+    }()
+    
+    private lazy var typeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.text = restaurant.type
+        label.textColor = .white
+        label.layer.cornerRadius = 5.0
+        label.layer.masksToBounds = true
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.backgroundColor = .orange
+        return label
+    }()
+    
+    private lazy var heartImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "heart-tick")
+        image.isHidden = !restaurant.isVisited
+        return image
+    }()
+    
+    private  var ratingImageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
     
     init(restaurant: RestaurantMO) {
         super.init(nibName: nil, bundle: nil)
@@ -51,9 +102,50 @@ class YQRestautrantDetailViewController: UIViewController {
     }
     
     func initDetailTableView() {
-        
+        view.addSubview(detailTableView)
+        initHeaderView()
+        initFooterView()
     }
     
+    private func initHeaderView() {
+        headerView.addSubview(headerMaskView)
+        headerView.addSubview(typeLabel)
+        headerView.addSubview(heartImageView)
+//        heartImageView.isHidden = !restaurant.isVisited
+        headerView.addSubview(nameLabel)
+        refreshRatingImageView(rateImage: restaurant.rating!)
+        headerView.addSubview(ratingImageView)
+        
+        detailTableView.tableHeaderView = headerView
+    }
+
+    private func initFooterView() {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: ScreenWidth(), height: 100))
+        
+        let rateButton = UIButton(frame: CGRect(x: Constants.leftMarginOfRateButton, y: 0, width: footerView.width() - Constants.leftMarginOfRateButton * 2, height: 47))
+        rateButton.center = CGPoint(x: rateButton.center.x, y: footerView.height() * 0.5)
+        rateButton.setTitle("Rate it", for: .normal)
+        rateButton.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        rateButton.titleLabel?.textColor = .white
+        rateButton.backgroundColor = .red
+        rateButton.layer.masksToBounds = true
+        rateButton.layer.cornerRadius = rateButton.frame.height * 0.5
+        rateButton.addTarget(self, action: #selector(handleClickRateButton), for: .touchUpInside)
+        footerView.addSubview(rateButton)
+        
+        detailTableView.tableFooterView = footerView
+    }
+
+    private func refreshRatingImageView(rateImage: String) {
+        self.restaurant.rating = rateImage
+        self.ratingImageView.image = UIImage(named: rateImage)
+        self.ratingImageView.sizeToFit()
+        self.view.setNeedsLayout()
+    }
+
+    @objc func handleClickRateButton() {
+        
+    }
     
 }
 
